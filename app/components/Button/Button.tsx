b83fe31c286cda1575e5a625ad57bdedf8d77346
Paulo-Dandrea <prog.dandrea@gmail.com>
2023-06-21 19:33:28 -0300

    feat: style login

---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,30 @@
+import styles from "./Button.module.css";
+
+interface ButtonProps {
+    color: "primary" | "alert" | "action" | "white";
+    onClick: () => void;
+    disabled?: boolean;
+    children: React.ReactNode;
+}
+
+export const Button = ({ color, onClick, disabled, children }: ButtonProps) => {
+    const colors = {
+        primary: styles.blue,
+        alert: styles.red,
+        action: styles.green,
+        white: styles.white,
+    };
+
+    const classes = [styles.btn, colors[color]].join(" ");
+
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            disabled={disabled}
+            className={classes}
+        >
+            {children}
+        </button>
+    );
+};
--- app/components/Login/Login.tsx
+++ app/components/Login/Login.tsx
@@ -1,76 +1,52 @@
 "use client";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { useDispatch, authSlice } from "@/lib/redux";
 import styles from "./Login.module.css";
+import { Button } from "../Button/Button";
 
 const Login = () => {
-  const dispatch = useDispatch();
-  const [username, setUsername] = useState("");
+    const dispatch = useDispatch();
+    const [username, setUsername] = useState("");
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
+    const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value);
+    };
 
-  const handleLogin = async () => {
-    dispatch(authSlice.actions.login(username));
-  };
+    const handleLogin = async () => {
+        dispatch(authSlice.actions.login(username));
+    };
 
-  return (
-    <div className={styles.modal}>
-      <div className={styles["modal-content"]}>
-        <h2>Welcome to CodeLeap network!</h2>
-        <form>
-          <div>
-            <label htmlFor="username">Please enter your username</label>
-            <br />
-            <input
-              type="text"
-              id="username"
-              value={username}
-              placeholder="John doe"
-              onChange={handleUsernameChange}
-            />
-          </div>
-          <div className={styles["button-container"]}>
-            <button type="button" onClick={handleLogin} disabled={!username}>
-              ENTER
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.modal}>
+            <div className={styles["modal-content"]}>
+                <h2>Welcome to CodeLeap network!</h2>
+                <form>
+                    <div>
+                        <label htmlFor="username">
+                            Please enter your username
+                        </label>
+                        <br />
+                        <input
+                            type="text"
+                            id="username"
+                            value={username}
+                            placeholder="John doe"
+                            onChange={handleUsernameChange}
+                        />
+                    </div>
+                    <div className={styles["button-container"]}>
+                        <Button
+                            color="primary"
+                            onClick={handleLogin}
+                            disabled={!username}
+                        >
+                            ENTER
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
-
-{
-  /* <div className="modal-content">
-<h2>Login</h2>
-<form>
-  <label>
-    Username:
-    <input
-      type="text"
-      value={username}
-      onChange={handleUsernameChange}
-      onKeyPress={handleKeyPress}
-    />
-  </label>
-  <br />
-  <label>
-    Password:
-    <input
-      type="password"
-      value={password}
-      onChange={handlePasswordChange}
-      onKeyPress={handleKeyPress}
-    />
-  </label>
-  <br />
-  <button type="button" onClick={handleLogin}>
-    Login
-  </button>
-</form>
-</div> */
-}
